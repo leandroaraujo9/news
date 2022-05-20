7eeaba4684fdfc057e2413c9b798538000045399
Leandro Araújo <leandro.araujo9@hotmail.com>
2022-05-20 16:35:26 -0300

    update(theme): Create custom fonts and colors

--- app/src/main/java/com/leandro/news/ui/theme/Type.kt
+++ app/src/main/java/com/leandro/news/ui/theme/Type.kt
@@ -2,27 +2,39 @@ package com.leandro.news.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.leandro.news.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+    Font(R.font.nexa_trial_black),
+    Font(R.font.nexa_trial_bold, weight = FontWeight.Bold),
+    Font(R.font.nexa_trial_light, weight = FontWeight.Light),
+    Font(R.font.nexa_trial_black, weight = FontWeight.Normal, style = FontStyle.Italic),
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fonts,
+        fontStyle = FontStyle.Italic,
+        fontSize = 16.sp
     )
-    */
 )
